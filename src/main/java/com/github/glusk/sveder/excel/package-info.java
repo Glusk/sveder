785c14509c35c3e81ce37c6e5cde9a3f4b9c35a8
Glusk2 <lj.slo@hotmail.com>
2020-09-05 15:30:04 +0200

    #52 Popravek dokumentacije paketa

--- src/main/java/com/github/glusk/sveder/excel/package-info.java
+++ src/main/java/com/github/glusk/sveder/excel/package-info.java
@@ -1,7 +1,6 @@
 /**
- * Razredi, ki znajo pridobiti podatke iz Excelovih preglednic, katere so
- * dostopne tukaj:
- * <a href="https://partner.zzzs.si/wps/portal/portali/aizv/zdravstvene_storitve/izbira_osebnega_zdravnika/sezn_akt_zob_za_odrasle_in_mladino">
- * ZZZS - Seznam aktivnih zobozdravnikov za odrasle in mladino</a>.
+ * Paket za delo z Excelovimi preglednicami, ki vsebujejo podatke o 
+ * zdravstvenih izvajalcih javne mreže in se nahajajo na spletnih
+ * straneh Zavoda za zdravstveno zavarovanje Slovenije.
  */
 package com.github.glusk.sveder.excel;
